da30fca674a32cceda17f51f51092c899e46d05b
Nikita <nikita-bat2000@mail.ru>
2019-02-19 00:07:19 +0500

    add lib for braille

---
+++ MAF/Braille.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAF
+{
+    /// <summary>
+    /// dont work
+    /// </summary>
+    class Braille
+    {
+        private string body;
+        private string code;
+
+        public Braille(string code)
+        {
+            body = code;
+            this.code = code.Select(x => ((char)(x + 2000))).ToString();
+        }
+    }
+}
